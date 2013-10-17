48e88229791124f73bfede2368d79c1d5bbbc70b
Tom van Dijk <t.vandijk@utwente.nl>
2013-10-17 15:43:28 +0200

    Replaced stress benchmark by dfs benchmark: repeatedly run depth first search on a balanced tree with parameterised depth, width and task size/grain

---
+++ benchmarks/dfs/dfs-lace.c
@@ -0,0 +1,90 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/time.h>
+#include <getopt.h>
+#include <lace-1.h>
+
+static int w, n;
+
+int __attribute__((noinline)) loop()
+{
+    int i, s=0;
+
+    for( i=0; i<n; i++ ) {
+        s += i;
+    }
+
+    return s;
+}
+
+VOID_TASK_1(tree, int, d)
+{
+    if( d>0 ) {
+        int i;
+        for (i=0;i<w;i++) SPAWN(tree, d-1);
+        for (i=0;i<w;i++) SYNC(tree);
+    } else {
+        loop();
+    }
+}
+
+double wctime()
+{
+    struct timeval tv;
+    gettimeofday(&tv, NULL);
+    return (tv.tv_sec + 1E-6 * tv.tv_usec);
+}
+
+void usage(char *s)
+{
+    fprintf(stderr, "%s -w <workers> [-q dqsize] <depth> <width> <grain> <reps>\n", s);
+}
+
+int main(int argc, char **argv)
+{
+    int workers = 1;
+    int dqsize = 100000;
+
+    char c;
+    while ((c=getopt(argc, argv, "w:q:h")) != -1) {
+        switch (c) {
+            case 'w':
+                workers = atoi(optarg);
+                break;
+            case 'q':
+                dqsize = atoi(optarg);
+                break;
+            case 'h':
+                usage(argv[0]);
+                break;
+            default:
+                abort();
+        }
+    }
+
+    if (optind + 3 >= argc) {
+        usage(argv[0]);
+        exit(1);
+    }
+
+    int d, m;
+    lace_init(workers, dqsize, 0);
+
+    d = atoi(argv[optind]);
+    w = atoi(argv[optind+1]);
+    n = atoi(argv[optind+2]);
+    m = atoi(argv[optind+3]);
+
+    printf("Running depth first search on %d balanced trees with depth %d, width %d, grain %d.\n", m, d, w, n);
+
+    double t1 = wctime();
+    int i;
+    for(i=0; i<m; i++) CALL(tree, d);
+    double t2 = wctime();
+
+    printf("Time: %f\n", t2-t1);
+
+    lace_exit();
+
+    return 0;
+}
---
+++ benchmarks/dfs/dfs-seq.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/time.h>
+#include <getopt.h>
+
+static int w, n;
+
+int __attribute__((noinline)) loop()
+{
+    int i, s=0;
+
+    for( i=0; i<n; i++ ) {
+        s += i;
+    }
+
+    return s;
+}
+
+void tree(int d)
+{
+    if( d>0 ) {
+        int i;
+        for (i=0;i<w;i++) tree(d-1);
+    } else {
+        loop();
+    }
+}
+
+double wctime()
+{
+    struct timeval tv;
+    gettimeofday(&tv, NULL);
+    return (tv.tv_sec + 1E-6 * tv.tv_usec);
+}
+
+void usage(char *s)
+{
+    fprintf(stderr, "%s <depth> <width> <grain> <reps>\n", s);
+}
+
+int main(int argc, char **argv)
+{
+    if (optind + 3 >= argc) {
+        usage(argv[0]);
+        exit(1);
+    }
+
+    int d, m;
+
+    d = atoi(argv[optind]);
+    w = atoi(argv[optind+1]);
+    n = atoi(argv[optind+2]);
+    m = atoi(argv[optind+3]);
+
+    printf("Running depth first search on %d balanced trees with depth %d, width %d, grain %d.\n", m, d, w, n);
+
+    double t1 = wctime();
+    int i;
+    for(i=0; i<m; i++) tree(d);
+    double t2 = wctime();
+
+    printf("Time: %f\n", t2-t1);
+
+    return 0;
+}
